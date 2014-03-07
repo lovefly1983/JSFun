8d006e98208b203524875d923b3d650abe7c5a8a
lovefly1983 <chunjie.wangcn@gmail.com>
2014-03-07 10:38:11 +0800

    Update template.js

--- template.js
+++ template.js
@@ -1,3 +1,16 @@
+//http://dmitrysoshnikov.com/ecmascript/javascript-the-core/#constructor
+//http://javascript.crockford.com/
+
 var b = function(a) {
     console.log('test : ' + a);
 }
+
+Object.prototype.createChild = function() {
+      function F() {}
+      F.prototype = this;
+      return new F();
+    };
+
+var parent = {name : 'abc'};
+var child = parent.createChild();
+console.log(child.name);
