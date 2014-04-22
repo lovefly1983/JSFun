c565aa286efb33a9faedb6ee51ce1f609911527b
lovefly1983 <chunjie.wangcn@gmail.com>
2014-04-22 10:02:38 +0800

    ab

--- template.js
+++ template.js
@@ -14,3 +14,45 @@ Object.prototype.createChild = function() {
 var parent = {name : 'abc'};
 var child = parent.createChild();
 console.log(child.name);
+
+
+
+function Test() {
+    var bbb = 111;
+    this.name = 'abc';
+    this.setName = function(_name) {
+      this.name = _name;
+    }
+    this.getName = function() {
+      return bbb;
+    }
+}
+
+function obj(val) {
+  this.name = 'abc';
+  this.x = val;
+}
+
+Test.prototype = new obj(333);
+
+var t = new Test();
+console.log(t.x);
+
+Array.prototype.testfun = function(x) {
+  console.log('ddddddddddd');
+}
+Array.prototype.b = 'ddd';
+var a = new Array();
+a[0] = 3;
+a.testfun(3);
+console.log(a.b);
+
+Function.prototype.output = function() {
+  console.log('output method');
+}
+
+function foo() {
+  console.log('foo');
+}
+
+foo.output();
